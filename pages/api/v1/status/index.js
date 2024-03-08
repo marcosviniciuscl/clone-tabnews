f06d0de80a9e34b4560d17653945664ac1e1ab17
Marcos Vinicius <marcosviniciuscl@outlook.com>
2024-03-08 22:58:35 +0000

    add new test

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -2,19 +2,31 @@ import database from "infra/database.js";
 
 async function status(resquest, response) {
   const updateAt = new Date().toISOString();
-  const resultQuery = await database.query("SHOW max_connections;");
-  const resultQueryConnections = await database.query(
-    "SELECT COUNT(*) AS connections_count FROM pg_stat_activity;",
-  );
-  const resultQueryVersion = await database.query("SELECT version();");
 
-  console.log(resultQuery.rows);
-  console.log(resultQueryConnections.rows);
-  response.status(200).json({
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseOpenedConnectionsResult = await database.query(
+    `SELECT COUNT(*)::int AS opened_connections FROM pg_stat_activity WHERE datname = '${process.env.POSTGRES_DB}';`,
+  );
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].opened_connections;
+
+  const resultQueryVersion = response.status(200).json({
     update_at: updateAt,
-    max_connections: resultQuery.rows[0].max_connections,
-    connections_count: resultQueryConnections.rows[0].connections_count,
-    version: resultQueryVersion.rows[0].version,
+    dependencies: {
+      database: {
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+        version: databaseVersionValue,
+      },
+    },
   });
 }
 
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,8 +3,13 @@ test("GET to /api/v1/status should returns 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.update_at).toBeDefined();
+
+  expect(responseBody.dependencies.database.version).toBe("16.0");
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 
   const parserUpdatedAt = new Date(responseBody.update_at).toISOString();
   expect(responseBody.update_at).toEqual(parserUpdatedAt);
+
+  console.log(responseBody);
 });
