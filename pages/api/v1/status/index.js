7209d7bf0a1031038289c4d51e137e40e26b974f
Marcos Vinicius <marcosviniciuscl@outlook.com>
2024-03-08 21:06:04 +0000

    update

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,21 @@
 import database from "infra/database.js";
 
 async function status(resquest, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: "OK" });
+  const updateAt = new Date().toISOString();
+  const resultQuery = await database.query("SHOW max_connections;");
+  const resultQueryConnections = await database.query(
+    "SELECT COUNT(*) AS connections_count FROM pg_stat_activity;",
+  );
+  const resultQueryVersion = await database.query("SELECT version();");
+
+  console.log(resultQuery.rows);
+  console.log(resultQueryConnections.rows);
+  response.status(200).json({
+    update_at: updateAt,
+    max_connections: resultQuery.rows[0].max_connections,
+    connections_count: resultQueryConnections.rows[0].connections_count,
+    version: resultQueryVersion.rows[0].version,
+  });
 }
 
 export default status;
